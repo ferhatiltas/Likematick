4c9924dc3fd4781cbc9d29c49a135dfa7a66cec6
Ferhat Iltas <iltasferhat@gmail.com>
2022-09-15 01:03:52 +0300

    Added onboarding screen

---
+++ likematick/Modules/Onboarding/Model/OnboardingModel.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingModel.swift
+//  likematick
+//
+//  Created by ferhatiltas on 14.07.2022.
+//
+
+import UIKit
+struct OnboardModel {
+    let title: String
+    let description: String
+    let image: UIImage
+}
